1e706d9350fc38da2f6b283e15fa6d749bfdff99
feyruz-loopcamp <feyruz@loopcamp.io>
2025-09-04 22:16:12 -0400

    Day 11

---
+++ src/test/java/io/loop/utilities/GeneralConstants.java
@@ -0,0 +1,14 @@
+package io.loop.utilities;
+
+public class GeneralConstants {
+
+    // browser types
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String SAFARI = "safari";
+
+    // herokuapp  url
+    public static final String HEROKUAPP_URL = "https://the-internet.herokuapp.com/forgot_password";
+
+
+}
