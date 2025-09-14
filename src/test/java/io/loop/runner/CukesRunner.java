5f393511681aafa537d3d5b15dba551c79d25b06
feyruz-loopcamp <feyruz@loopcamp.io>
2025-09-14 14:43:39 -0400

    day 24

--- src/test/java/io/loop/runner/CukesRunner.java
+++ src/test/java/io/loop/runner/CukesRunner.java
@@ -11,9 +11,23 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "io/loop/step_definitions",
         dryRun = false,
-        tags = "@listOfMap",
-        monochrome = true
+        tags = "@mapListProduct",
+        monochrome = true,
+        publish = false
 )
+
+/*
+junit 5 version
+@Suite
+@SelectClasspathResource("features")   // looks under src/test/resources/features
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io/loop/step_definitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "html:target/html-reports/cucumber-report.html, json:target/json-reports/json-report.json")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@listOfMap")
+@ConfigurationParameter(key = MONOCHROME_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+ */
 
 public class CukesRunner {
 }
